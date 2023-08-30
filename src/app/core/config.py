b237be694986f6b590e6ae701e191c68f162d3d5
Pavel Khramov <102523130+KhramovKhramov@users.noreply.github.com>
2023-08-31 01:20:50 +0500

    feature: postgres add to docker

--- src/app/core/config.py
+++ src/app/core/config.py
@@ -1,6 +1,6 @@
 import os
 
-from pydantic import Field
+from pydantic import Field, PostgresDsn
 from pydantic_settings import BaseSettings
 
 
@@ -11,8 +11,29 @@ class ProjectConfig(BaseSettings):
     jwt_secret_key: str = Field(default="secret_key", alias="JWT_SECRET_KEY")
 
 
+# Настройки PostgreSQL
+class PostgresConfig(BaseSettings):
+    host: str = Field(default='localhost', alias='POSTGRES_HOST')
+    port: int = Field(default=5432, alias='POSTGRES_HOST')
+    db: str = Field(default='tasks', alias='POSTGRES_DB')
+    user: str = Field(default='postgres', alias='POSTGRES_USER')
+    password: str = Field(default='postgres', alias='POSTGRES_PASSWORD')
+
+    @property
+    def dsn(self) -> str:
+        return PostgresDsn.build(
+            scheme="postgresql+asyncpg",
+            user=self.user,
+            password=self.password,
+            host=self.host,
+            port=str(self.port),
+            path=f"/{self.db}",
+        )
+
+
 class Settings(BaseSettings):
     project: ProjectConfig = ProjectConfig()
+    postgres: PostgresConfig = PostgresConfig()
 
 
 settings = Settings()
