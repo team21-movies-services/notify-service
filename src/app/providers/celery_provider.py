67fb5f19f78125a889f010cf466ea561c2db382c
Leonid Bakhmetev <leonidbkh@pm.me>
2023-08-30 04:36:05 +0400

    feature: celery provider and config added

---
+++ src/app/providers/celery_provider.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI
+
+from providers import BaseProvider
+from core.config import Settings
+from celery import Celery
+
+
+class CeleryProvider(BaseProvider):
+    def __init__(self, app: FastAPI, settings: Settings):
+        self.app = app
+        self.celery_client = Celery(
+            broker=settings.celery.broker,
+            backend=settings.celery.backend,
+            app=settings.celery.app_name,
+        )
+
+    def startup(self):
+        """FastAPI startup event"""
+        setattr(self.app.state, "celery_provider", self.celery_client)
+
+    def shutdown(self):
+        """FastAPI shutdown event"""
+        if self.celery_client:
+            self.celery_client.close()
--- src/app/providers/main.py
+++ src/app/providers/main.py
@@ -3,8 +3,12 @@ import logging
 from core.config import Settings
 from fastapi import FastAPI
 
+from providers.celery_provider import CeleryProvider
+
 logger = logging.getLogger(__name__)
 
 
 def setup_providers(app: FastAPI, settings: Settings):
-    pass
+    celery_provider = CeleryProvider(app=app, settings=settings)
+    celery_provider.register_events()
+    logger.info(f"Setup Celery Provider. host: {settings.celery.host}")
