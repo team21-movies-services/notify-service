54bc553f3f4120bff3555a8bcd0189f838055071
Leonid Bakhmetev <leonidbkh@pm.me>
2023-08-30 04:56:07 +0400

    feature: test endpoint for celery tasks

--- src/app/api/routers/v1/status.py
+++ src/app/api/routers/v1/status.py
@@ -1,6 +1,6 @@
 import logging
 
-from fastapi import APIRouter, Depends, status
+from fastapi import APIRouter, Depends, status, Request
 from schemas.response.status import StatusResponse
 from services.status import StatusServiceABC
 
@@ -20,3 +20,12 @@ async def _get_api_status(
 ) -> StatusResponse:
     logger.debug("Get api status")
     return await status_service.get_api_status()
+
+
+@router.get(
+    path="",
+    status_code=status.HTTP_200_OK,
+)
+async def debug_celery(request: Request):
+    task = request.app.state.celery_provider.send_task("debug_task", (1, 2))
+    return task.get()
