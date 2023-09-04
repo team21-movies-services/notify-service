ec9e08905f898b67d82f567279cbe8008a933d7b
Mikhail Spiridonov <mspiridonov2706@gmail.com>
2023-09-04 15:55:07 +0200

    chore: update process event;

--- src/celery_worker/services/notify.py
+++ src/celery_worker/services/notify.py
@@ -5,9 +5,8 @@ from typing import NoReturn
 from celery_worker.exceptions.events import EventCouldNotBeHadnled
 from celery_worker.repositories import TemplatesRepository
 from celery_worker.utils.handlers_factory import HandlerFactoryProtocol
-from shared.enums.events import EventNameEnum
 from shared.enums.notifications import NotificationTypesEnum
-from shared.schemas.events import EventSchema
+from shared.schemas.events import EventFilmsNewSchema, EventSchema, EventUsersNewSchema
 
 logger = logging.getLogger(__name__)
 
@@ -18,19 +17,21 @@ class NotifyService:
     _handlers_factory: HandlerFactoryProtocol
 
     def process_event(self, event: EventSchema) -> None:
-        match event.event_name:
-            case EventNameEnum.users_created:
-                logger.info(EventNameEnum.users_created)
-            case EventNameEnum.films_new:
-                logger.info(EventNameEnum.films_new)
+        match event.event_data:
+            case EventUsersNewSchema():  # type: ignore[misc]
+                users_list = [event.event_data.user_id]
+                logger.info(users_list)
+            case EventFilmsNewSchema():  # type: ignore[misc]
+                # users_list = self._content_service.get_users_list(event.event_name)
+                logger.info(event.event_name)
             case _ as unreachable:
                 _asset_never(unreachable)
 
         notify_handler = self._handlers_factory.get(NotificationTypesEnum.email)
         logger.info(notify_handler)
 
-        # notify_handler.render()
-        # notify_handler.send()
+        # rendered_template = notify_handler.render(content, template_body, template_wrapper)
+        # notify_handler.send(users_list, rendered_template)
 
 
 def _asset_never(event_name: str) -> NoReturn:
