1aab6fe7a15367869e872dd8e96ce0f18b8487ab
Pavel Khramov <102523130+KhramovKhramov@users.noreply.github.com>
2023-09-06 22:37:18 +0500

    feature: add sentry

--- src/app/providers/main.py
+++ src/app/providers/main.py
@@ -6,6 +6,7 @@ from app.core.config import Settings
 from app.providers.admin_provider import AdminProvider
 from app.providers.celery_provider import CeleryProvider
 from app.providers.pg_providers import SQLAlchemyProvider
+from app.providers.sentry_provider import SentryProvider
 
 logger = logging.getLogger(__name__)
 
@@ -30,3 +31,8 @@ def setup_providers(app: FastAPI, settings: Settings):
     )
     admin_provider.register_events()
     logger.info("Setup Admin Provider.")
+
+    if settings.sentry.enable:
+        sentry_provider = SentryProvider(app=app, dsn=settings.sentry.dsn)
+        sentry_provider.register_events()
+        logger.info("Setup Sentry Provider.")
