6c8b64b0ec2500292eba8fd57bcf5b5b8903fe0e
Leonid Bakhmetev <leonidbkh@pm.me>
2023-09-09 15:09:01 +0400

    feature: dependencies added, simple test added

--- src/tests/functional/src/status/test_status.py
+++ src/tests/functional/src/status/test_status.py
@@ -1,7 +1,25 @@
+from http import HTTPStatus
+
 import pytest
 
 
+@pytest.mark.parametrize(
+    ("route", "params", "json", "expected_status"),
+    [
+        ("/api/v1/status", None, None, HTTPStatus.NOT_FOUND),
+        (
+            "/api/v1/events",
+            None,
+            {
+                "event_data": {"email": "123@example.com", "user_id": "4042222c-07c2-4316-8b4b-a0cbc4ece7b6"},
+                "event_name": "users.created",
+                "event_time": "123",
+            },
+            HTTPStatus.NOT_FOUND,  # тут так пока пустая база
+        ),
+    ],
+)
 @pytest.mark.asyncio()
-async def test_healthcheck(api_client):
-    response = await api_client.post("/api/v1/status")
-    assert response.status_code == 200
+async def test_status(api_client, route, params, expected_status, json):
+    response = await api_client.post(route, json=json)
+    assert response.status_code == expected_status
